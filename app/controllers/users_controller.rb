6a4c72347674e30ae2b2cec4256d01b1d38f7ba4
supiash1 <supiash@gmail.com>
2024-06-21 14:56:51 +0100

    Refactor user import to use S3 for file uploads due to Heroku's ephemeral filesystem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  # TODO: Modify the import method to upload the CSV file directly to S3 and read data from there,
+  # as Heroku's filesystem is ephemeral and not suitable for direct file uploads.
   def import
     @user_imports = UsersImport.new(CsvFileReader.new(params[:file].path)).call
     respond_to(&:turbo_stream)
